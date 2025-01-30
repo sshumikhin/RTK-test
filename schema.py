ffe6f09933cf6b6b45f3e9a0f7a0911769c59234
Alexandr Shumikhin <shumikhin03@inbox.ru>
2025-01-30 15:35:00 +0300

    UPDATE: validation to json model

--- schema.py
+++ schema.py
@@ -1,23 +1,25 @@
-# Standart
 from typing import Optional, List
-
-# Third party
-from pydantic import BaseModel, field_validator
-
-# First party
+from pydantic import BaseModel, field_validator, constr, conint
 from exceptions import ModelValidateError
 
-
 class ConfigurationParameters(BaseModel):
-    username: str
-    password: str
-    vlan: Optional[int]
-    interfaces: List[int]
+    username: constr(min_length=1)
+    password: constr(min_length=1)
+    vlan: Optional[conint(ge=0)]
+    interfaces: List[conint(ge=0)]
+
+    @field_validator('interfaces', mode='before')
+    @classmethod
+    def validate_interfaces(cls, interfaces):
+        if not interfaces:
+            raise ModelValidateError("Interfaces list cannot be empty")
+        return interfaces
 
 
 class ConfigurationRequest(BaseModel):
-    timeoutInSeconds: int
+    timeoutInSeconds: conint(ge=0)
     parameters: List[ConfigurationParameters]
+
 
     @field_validator("timeoutInSeconds", mode='before')
     @classmethod
@@ -26,3 +28,9 @@ class ConfigurationRequest(BaseModel):
             raise ModelValidateError("Incorrect timeout value")
         return timeout
 
+    @field_validator("parameters", mode='before')
+    @classmethod
+    def validate_parameters(cls, parameters):
+        if not parameters:
+            raise ModelValidateError("Parameters list cannot be empty")
+        return parameters

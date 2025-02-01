26608fe9e9c27c30608a8e999daf85ddddb3ad69
Alexandr Shumikhin <shumikhin03@inbox.ru>
2025-02-01 20:53:16 +0300

    UPDATE: more logging and docs

--- script.py
+++ script.py
@@ -1,20 +1,39 @@
-import asyncio
+"""
+    Скрипт, взаимодействующий с брокером сообщений и сервисом А
+"""
+
+# Standart
 import os
 import sys
+import json
+import logging
+
+# Third party
+import asyncio
 import redis.asyncio as aioredis
 from redis.exceptions import ResponseError
 import aiohttp
-import json
-import logging
 from dotenv import load_dotenv
 
 
 load_dotenv()
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger("Script")
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
 
+file_handler = logging.FileHandler('errors.log')
+file_handler.setLevel(logging.ERROR)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
 
 HOST = os.getenv("REDIS_HOST", "localhost")
 PORT = os.getenv("REDIS_PORT", 6379)
@@ -27,12 +46,19 @@ SERVICE_A = os.getenv("SERVICE_A", "localhost:8000")
 
 
 class ServiceA:
+    """
+        Класс для работы с сервисом А
+    """
 
     BASE_URL = f'http://{SERVICE_A}/api/v1/equipment/cpe/'
     headers = {'Content-Type': 'application/json'}
 
     async def configure_equipment_by_task_from_broker(self, task: dict):
+        """
+            Метод для конфигурации оборудования
 
+            Имплементирует POST /api/v1/equipment/cpe/
+        """
         data = {
             "timeoutInSeconds": task["timeout_in_seconds"],
             "parameters": [
@@ -55,12 +81,18 @@ class ServiceA:
                     return await response.json()
             except aiohttp.ClientError as e:
                 logger.error(f"Сервис A не доступен: {e}")
-                return None
+                sys.exit(1)
 
 
 class TaskManager:
+    """
+        Класс, реализующий все функции скрипта
+    """
 
     def __init__(self):
+        """
+            Метод-инициализатор, создающий соединение с Redis
+        """
         try:
             self.redis = aioredis.from_url(
                 url=CONNECTION_URL,
@@ -72,6 +104,13 @@ class TaskManager:
             sys.exit(1)
 
     async def connect_to_consumers_group(self):
+        """
+            Метод, создающий группу и подключающийся в Redis Stream к потоку
+
+            В случае если группа уже существует, то она не создается
+
+            Имплементирует XGROUP CREATE
+        """
         try:
             await self.redis.xgroup_create(CONSUMER_STREAM_NAME, CONSUMER_GROUP_NAME, mkstream=True)
         except ResponseError as error:
@@ -79,32 +118,44 @@ class TaskManager:
                 logger.info(f"Группа {CONSUMER_GROUP_NAME} уже существует")
             else:
                 logger.error(f"Ошибка при создании группы : {error}")
+                sys.exit(1)
 
     async def check_tasks_from_service_b(self, stream_viewing_type: str = '0'):
-        messages = await self.redis.xreadgroup(
-            groupname=CONSUMER_GROUP_NAME,
-            consumername=CONSUMER_NAME,
-            streams={CONSUMER_STREAM_NAME: stream_viewing_type},
-            block=5000
-        )
+        """
+            Метод, получающий задания из сервиса B
 
-        tasks = {}
+            Имплементирует XREADGROUP
+        """
+        try:
+            messages = await self.redis.xreadgroup(
+                groupname=CONSUMER_GROUP_NAME,
+                consumername=CONSUMER_NAME,
+                streams={CONSUMER_STREAM_NAME: stream_viewing_type},
+                block=5000
+            )
 
-        if bool(messages):
-            for msg_id, msg_body in messages[0][1]:
+            tasks = {}
 
-                try:
-                    task = json.loads(msg_body["message"])
-                except KeyError:
-                    logger.error(f"Отсутствует поле message в сообщении {msg_body}")
-                    return
+            if bool(messages):
+                for msg_id, msg_body in messages[0][1]:
 
-                tasks[msg_id] = task
-                logger.info(f"Получено задание {task["taskId"]} из канала {CONSUMER_STREAM_NAME}")
+                    try:
+                        task = json.loads(msg_body["message"])
+                    except KeyError:
+                        logger.error(f"Отсутствует поле message в сообщении {msg_body}")
+                        sys.exit(1)
 
-        return tasks
+                    tasks[msg_id] = task
+                    logger.info(f"Получено задание {task["taskId"]} из потока {CONSUMER_STREAM_NAME}")
+            return tasks
+        except Exception as e:
+            logger.error("Не удалось получить сообщения из сервиса B")
+
 
     async def send_data_to_service_a(self, task: dict):
+        """
+            Метод для отправки задания в сервис А
+        """
         result = await ServiceA().configure_equipment_by_task_from_broker(
             task=task
         )
@@ -112,49 +163,82 @@ class TaskManager:
         return result
 
     async def send_completed_task_to_broker(self, task_id):
-        await self.redis.xadd(RECEIVER_STREAM_NAME, {"taskId": task_id})
+        """
+            Метод для отправки задания в канал для получателя(Сервис B)
+
+            Имплементирует XADD
+        """
+        try:
+            await self.redis.xadd(RECEIVER_STREAM_NAME, {"taskId": task_id})
+        except Exception as e:
+            logger.error(f"Не удалось отправить задание в канал {RECEIVER_STREAM_NAME}: {e}")
+            logger.error(f"task_id: {task_id}")
+            sys.exit(1)
 
     async def confirm_receipt(self, message_id):
-        await self.redis.xack(CONSUMER_STREAM_NAME, "processing_group", message_id)
+        """
+            Метод для подтверждения получения и обработки задания
+
+            Имплементирует XACK
+        """
+        try:
+            await self.redis.xack(CONSUMER_STREAM_NAME, "processing_group", message_id)
+        except Exception as e:
+            logger.error(f"Не удалось подтвердить получение задания: {e}")
+            logger.error(f"message_id: {message_id}")
+            sys.exit(1)
 
     async def work(self):
+        """
+            Метод, реализующий цикл обработки заданий
+
+            В данном методе применяются все вышеперечисленные методы.
+
+            Метод описывает работу скрипта
+        """
         await self.connect_to_consumers_group()
-        old_tasks = await self.check_tasks_from_service_b()
 
-        if old_tasks:
-            logger.info(f"Получено {len(old_tasks)} старых заданий из канала {CONSUMER_STREAM_NAME}")
-            for message_id, task in old_tasks.items():
-                logger.info(f"Отправка задания {task['taskId']} в сервис А")
-                result = await self.send_data_to_service_a(
-                    task=task
-                )
-
-                if result["message"] == "success":
-                    await self.send_completed_task_to_broker(task["taskId"])
-                await self.confirm_receipt(message_id=message_id)
-                logger.info(f"Задание {task['taskId']} обработано")
-        else:
-            logger.info(f"Нет старых заданий в канале {CONSUMER_STREAM_NAME}")
+        first_run = True
 
         while True:
-            new_tasks = await self.check_tasks_from_service_b(stream_viewing_type='>')
+            if first_run:
+                new_tasks = await self.check_tasks_from_service_b()
+                first_run = False
+            else:
+                new_tasks = await self.check_tasks_from_service_b(stream_viewing_type='>')
+
             if new_tasks:
-                logger.info(f"Получено {len(old_tasks)} заданий из канала {CONSUMER_STREAM_NAME}")
+                logger.info(f"Получено {len(new_tasks)} заданий из канала {CONSUMER_STREAM_NAME}")
                 for message_id, task in new_tasks.items():
                     logger.info(f"Отправка задания {task['taskId']} в сервис А")
                     result = await self.send_data_to_service_a(
-                        task=task
+                            task=task
                     )
-                    logging.info(result)
-                    if result["message"] == "success":
+                    # TODO: для каждого кода свой флаг в completed_task
+                    if result["code"] == 200:
+                        await self.confirm_receipt(message_id=message_id)
                         await self.send_completed_task_to_broker(task["taskId"])
+                        logger.info(f"Задание {task['taskId']} обработано")
+
+                    elif result["code"] == 404:
+                        await self.send_completed_task_to_broker(task["taskId"])
+                        await self.confirm_receipt(message_id=message_id)
+                        logger.info(f"Задание {task['taskId']} не было найдено")
+
+                    else:
+                        logger.error("Сервис A не доступен")
+                        logger.error(f"task_id: {task['taskId']}")
+                        # Не подтвердили обработку задачи, а значит, она остаётся в брокере
+                        continue
                     await self.confirm_receipt(message_id=message_id)
-                    logger.info(f"Задание {task['taskId']} обработано")
             else:
                 logger.info(f"Нет новых заданий в канале {CONSUMER_STREAM_NAME}")
 
 
 async def main():
+    """
+        Запуск скрипта
+    """
 
     task_manager = TaskManager()
     try:
@@ -166,6 +250,7 @@ async def main():
     finally:
         await task_manager.redis.aclose()
         logger.info("Завершение работы скрипта")
+
 
 if __name__ == "__main__":
     asyncio.run(main())
